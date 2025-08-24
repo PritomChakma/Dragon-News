82a530fe3f22070c5aa640ea84bf946353287f79
Pritom Chakma <chakmapritom1@gmail.com>
2025-08-24 22:41:37 +0600

    Create navbar and mani three grid

--- src/Root.jsx
+++ src/Root.jsx
@@ -1,13 +1,24 @@
 import Header from "./Components/Header/Header";
 import LeatestNews from "./Components/Leatest/LeatestNews";
+import NavBar from "./Components/NavBar/NavBar";
 
 function Root() {
   return (
     <>
-      <header><Header></Header></header>
-      <section><LeatestNews></LeatestNews></section>
-      <nav></nav>
-      <main></main>
+      <header>
+        <Header></Header>
+      </header>
+      <section>
+        <LeatestNews></LeatestNews>
+      </section>
+      <nav>
+        <NavBar></NavBar>
+      </nav>
+      <main className="w-11/12 mx-auto grid grid-cols-12">
+        <aside className="col-span-3">Left</aside>
+        <main className="col-span-6">Main</main>
+        <aside className="col-span-3">Right</aside>
+      </main>
       <footer></footer>
     </>
   );
